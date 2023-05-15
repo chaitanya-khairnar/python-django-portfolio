ccac6dc227a8800d4d122abbb5df8c9ebf9b63c7
chaitanya-khairnar <chaitanyahemant.khairnar@gmail.com>
2023-05-15 15:59:54 +0100

    added web resume

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from home.models import General_info, Project, Jobs, Experience
+from home.models import (
+    General_info,
+    Project,
+    Jobs,
+    Experience,
+    Education
+)
 
 
 # Register your models here.
@@ -7,3 +13,4 @@ admin.site.register(General_info)
 admin.site.register(Project)
 admin.site.register(Jobs)
 admin.site.register(Experience)
+admin.site.register(Education)
