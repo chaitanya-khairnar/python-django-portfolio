7b23908068924cbb4a8989cca6fc1dcdc7ec467f
chaitanya-khairnar <80409743+chaitanya-khairnar@users.noreply.github.com>
2023-06-08 10:20:30 +0100

    Update prod.py

--- portfolio/settings/prod.py
+++ portfolio/settings/prod.py
@@ -7,4 +7,4 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['prod-server-ips']
+ALLOWED_HOSTS = ['127.0.0.1']
